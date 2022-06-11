942f18055a7abf5a2a9b8d3751cafa68d2d3a186
anupam@pc <pandey.anupam@hotmail.com>
2022-06-12 02:04:22 +0530

    trying broker micro service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"fmt"
+)
+
+const webPort = "8080"
+
+type Config struct {}
+
+func main()  {
+
+	app := Config{}
+	log.Printf("Starting broker services on port %s",webPort)
+	
+	// define http server
+	
+	srv := &http.Server{
+		Addr: fmt.Sprintf(" : %s",webPort),
+		Handler: app.routes(),
+	}
+
+	//start server
+
+	err := srv.ListenAndServe()
+	if err!=nil{
+		log.Panic(err)
+	}
+}
