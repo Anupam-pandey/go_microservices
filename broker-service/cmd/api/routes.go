f0675c19469041dd5d87c13c0629a73c28a5c27e
anupam@pc <pandey.anupam@hotmail.com>
2022-06-12 21:44:05 +0530

    trying broker micro service

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/cors"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 //specify who is allowed to connect
 //https://github.com/go-chi/cors
 
-func (app *Config) routes() http.Handler{
+func (app *Config) routes() http.Handler {
 
 	mux := chi.NewRouter()
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"https://*","http://*"},
-		AllowedMethods: []string{"GET","POST","PUT","HEAD","DELETE","OPTIONS"},
-		AllowedHeaders: []string{"Accept","Authorization", "Content-Type","X-CSRF-Token"},		
-		ExposedHeaders: []string{"Link"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "HEAD", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
-		MaxAge:300,
+		MaxAge:           300,
 	}))
 
 	mux.Use(middleware.Heartbeat("/ping"))
-	mux.Post("/",app.Broker)
+	mux.Post("/", app.Broker)
 	return mux
 }
